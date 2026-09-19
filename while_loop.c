745078443d319351619b530c81ebea51d7ec2e3e
omprasadk45-prog <omprasadk45@gmail.com>
2026-09-20 00:09:05 +0530

    add while_loop.c

--- while_loop.c
+++ while_loop.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+// reverse star with spaces
+/* #include <stdio.h>
 int main(){
     int x=1, y, z;
     while(x<=5){
@@ -15,5 +16,63 @@ int main(){
         printf("\n");
         x++;
     }
+    return 0;
+}
+
+//pyramid pattern
+
+#include <stdio.h>
+int main(){
+    int x=1, y, z;
+    while("x<=4"){
+        y=1;
+        while(y=4-x){
+            printf(" ");
+            y++;
+            z=1;
+        }
+        z=1;
+        while(z<=2*x-1){
+            printf("*");
+            z++;
+        }
+        x++;
+    } 
+    return 0;
+}
+
+
+//number triangle
+
+//incomplete
+#include <stdio.h> 
+int main(){
+    int x=1, y;
+    while(x<=5){
+        y=1;
+        while(y<x){
+            printf("%d",y++);
+            y++;
+        }
+    printf("\n");
+    x++;    
+    }
+    return 0;
+}*/
+
+
+
+
+//factorial
+#include <stdio.h>
+int main(){
+    int x=1,n, factorial=1;
+    printf("enter the factorial to be calculated");
+    scanf("%d",&n);
+    while(x<=n){
+        factorial=factorial*x;
+        x++;
+    } 
+    printf("%d",factorial);
     return 0;
 }
