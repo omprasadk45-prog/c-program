e144fa53ea502246e7444e51cd5db6e199aa5693
omprasadk45-prog <omprasadk45@gmail.com>
2026-09-21 19:15:53 +0530

    add while_loop

--- while_loop.c
+++ while_loop.c
@@ -58,7 +58,7 @@ int main(){
     x++;    
     }
     return 0;
-}*/
+}
 
 
 
@@ -75,4 +75,61 @@ int main(){
     } 
     printf("%d",factorial);
     return 0;
+}
+
+//alphabet ladder
+#include <stdio.h>
+int main(){
+    int x = 1,y;
+    char ch;
+    while(x<=5){
+        y=1;
+        ch= 'a';// this declaration gives a ab abc.....
+        while(y<=x){
+            printf("%c",ch);
+            y++;
+            ch++;
+        }
+        printf("\n");
+        x++;
+    }
+    return 0;
+}
+
+
+// alphabet ladder 2
+#include <stdio.h>
+int main(){
+    int x = 1,y;
+    char ch='a'; //this declaration gives a bc def...
+    while(x<=5){
+        y=1;
+        while(y<=x){
+            printf("%c",ch);
+            y++;
+            ch++;
+        }
+        printf("\n");
+        x++;
+    }
+    return 0;
+}*/
+
+//fibonacci sequence
+
+#include <stdio.h>
+int main(){
+    int x=0,n,y=1;
+    printf("enter the number of terms");
+    scanf("%d",&n);
+    printf("0\n");
+    while(x==0){
+    printf("%d\n",++x);
+    x++;
+        while(y<=n){
+            printf("%n",x+y);
+            y++;
+        }
+    }
+return 0;
 }
