af953edbd316a59e9832bf911f525e36dcf69545
omprasadk45-prog <omprasadk45@gmail.com>
2026-09-23 00:15:14 +0530

    add for_loop

--- for_loop.c
+++ for_loop.c
@@ -8,7 +8,37 @@ int main(){
 return 0;
 }*/
 
+
+//* laddder
+/*#include <stdio.h>
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}*/
+// alphabet ladder
+
+
+/*#include <stdio.h>
+int main(){
+    int i=1,j;
+    for(i=0;i<5;i++){
+        for(j=0;j<=i;j++){
+            printf("%c",'A'+ j); //i controls the letter j controls the number of times the lettr repeats
+        }
+        printf("\n");
+    }
+return 0;
+}*/
+
+
 #include <stdio.h>
 int main(){
     
 }
+
